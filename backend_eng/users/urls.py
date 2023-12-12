51774129a0b5ddaca1c3d55b0be8e3d83070f3db
Neetesh NG <neeteshng15@gmail.com>
2023-12-12 17:42:49 +0530

    Included some things in Settings and Migrated It.

--- backend_eng/users/urls.py
+++
@@ -1,9 +0,0 @@
-# users/urls.py
-
-from django.urls import path
-from .views import register, login
-
-urlpatterns = [
-    path('register/', register, name='register'),
-    path('login/', login, name='login'),
-]
