307ac4b8d03b38057299432bfa7602b60c6dca1b
Neetesh NG <neeteshng15@gmail.com>
2023-12-12 15:04:54 +0530

    Created Models for the User Fields.

--- backend_eng/users/models.py
+++ backend_eng/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    )
+
+    phone_number = models.CharField(max_length=15)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
+    def __str__(self):
+        return self.username
