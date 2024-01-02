616bded0f45ffae6044eb681c1f5ebfaad7ea774
Neetesh NG <neeteshng15@gmail.com>
2024-01-02 10:13:46 +0530

    Wishlist

--- backend_api/wishlist/urls.py
+++ backend_api/wishlist/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import WishlistItemListCreateView, WishlistItemDetailView
+from .views import WishlistItemListCreateView, WishlistItemDetailView, WishlistItemDeleteView
 
 urlpatterns = [
     path('wishlist-items/', WishlistItemListCreateView.as_view(), name='wishlist-item-list'),
     path('wishlist-items/<int:pk>/', WishlistItemDetailView.as_view(), name='wishlist-item-detail'),
+    path('wishlist-items/delete/<int:pk>/', WishlistItemDeleteView.as_view(), name='wishlist-item-delete'),
 ]
--- backend_api/wishlist/views.py
+++ backend_api/wishlist/views.py
@@ -12,7 +12,7 @@ class WishlistItemDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = WishlistItem.objects.all()
     serializer_class = WishlistItemSerializer
 
-class AddToCartView(generics.CreateAPIView):
+class AddToWishlistView(generics.CreateAPIView):
     serializer_class = WishlistItemSerializer
 
     def post(self, request, *args, **kwargs):
@@ -21,17 +21,37 @@ class AddToCartView(generics.CreateAPIView):
         
         if user and product_id:
             try:
-                # Check if the product already exists in the user's cart
-                cart_item = WishlistItem.objects.filter(user=user, product_id=product_id).first()
+                wishlist_Item = WishlistItem.objects.filter(user=user, product_id=product_id).first()
                 
-                if cart_item:
-                    cart_item.save()
-                    serializer = WishlistItemSerializer(cart_item)
+                if wishlist_Item:
+                    wishlist_Item.save()
+                    serializer = WishlistItemSerializer(wishlist_Item)
                     return Response(serializer.data, status=status.HTTP_200_OK)
                 else:
-                    cart_item = WishlistItem.objects.create(user=user, product_id=product_id)
-                    serializer = WishlistItemSerializer(cart_item)
+                    wishlist_Item = WishlistItem.objects.create(user=user, product_id=product_id)
+                    serializer = WishlistItemSerializer(wishlist_Item)
                     return Response(serializer.data, status=status.HTTP_201_CREATED)
+            except Exception as e:
+                return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'error': 'Invalid data'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class WishlistItemDeleteView(generics.DestroyAPIView):
+    queryset = WishlistItem.objects.all()
+    serializer_class = WishlistItemSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        user = request.user
+        product_id = request.data.get('productId')
+        
+        if user and product_id:
+            try:
+                wishlist_item = WishlistItem.objects.get(user=user, product_id=product_id)
+                wishlist_item.delete()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            except WishlistItem.DoesNotExist:
+                return Response({'error': 'Item does not exist'}, status=status.HTTP_404_NOT_FOUND)
             except Exception as e:
                 return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
         else:
