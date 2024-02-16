c50c73b75dc615d298576a9b8201e1c41d14c1ab
NeeteshNG <neeteshng15@gmail.com>
2024-02-16 12:41:26 +0530

    Add to Cart and Remove from Cart Functionality

--- backend_api/cart/urls.py
+++ backend_api/cart/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CartItemListCreateView, CartItemDetailView
+from .views import CartItemListCreateView, RemoveFromCart, AddToCartView
 
 urlpatterns = [
     path('cart-items/', CartItemListCreateView.as_view(), name='cart-item-list'),
-    path('cart-items/<int:pk>/', CartItemDetailView.as_view(), name='cart-item-detail'),
+    path('cart-items/<int:pk>/', RemoveFromCart.as_view(), name='cart-item-detail'),
+    path('add-to-cart/', AddToCartView.as_view(), name='add-to-cart'),
 ]
--- backend_api/cart/views.py
+++ backend_api/cart/views.py
@@ -3,39 +3,51 @@ from .models import CartItem
 from .serializers import CartItemSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 class CartItemListCreateView(generics.ListCreateAPIView):
     queryset = CartItem.objects.all()
     serializer_class = CartItemSerializer
 
-class CartItemDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = CartItem.objects.all()
-    serializer_class = CartItemSerializer
+    def get_queryset(self):
+        user = self.request.user
+        return CartItem.objects.filter(user=user)
+
 
 class AddToCartView(generics.CreateAPIView):
     serializer_class = CartItemSerializer
+    permission_classes = [IsAuthenticated]
 
     def post(self, request, *args, **kwargs):
         user = request.user
-        product_id = request.data.get('productId')
+        product_id = request.data.get('product')
         
-        if user and product_id:
-            try:
-                # Check if the product already exists in the user's cart
-                cart_item = CartItem.objects.filter(user=user, product_id=product_id).first()
-                
-                if cart_item:
-                    # If the item exists, increment the quantity
-                    cart_item.quantity += 1
-                    cart_item.save()
-                    serializer = CartItemSerializer(cart_item)
-                    return Response(serializer.data, status=status.HTTP_200_OK)
-                else:
-                    # If the item does not exist, create a new cart item with quantity=1
-                    cart_item = CartItem.objects.create(user=user, product_id=product_id, quantity=1)
-                    serializer = CartItemSerializer(cart_item)
-                    return Response(serializer.data, status=status.HTTP_201_CREATED)
-            except Exception as e:
-                return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            return Response({'error': 'Invalid data'}, status=status.HTTP_400_BAD_REQUEST)
+        if not product_id:
+            return Response({'error': 'productId is required'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            # Check if the product already exists in the user's cart
+            cart_item = CartItem.objects.filter(user=user, product=product_id).first()
+            
+            if cart_item:
+                # If the item exists, increment the quantity
+                cart_item.quantity += 1
+                cart_item.save()
+                serializer = CartItemSerializer(cart_item)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                # If the item does not exist, create a new cart item with quantity=1
+                cart_item = CartItem.objects.create(user=user, product_id=product_id, quantity=1)
+                serializer = CartItemSerializer(cart_item)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+class RemoveFromCart(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return CartItem.objects.filter(user=user)
