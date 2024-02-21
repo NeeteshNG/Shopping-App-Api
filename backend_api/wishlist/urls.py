eb90b41636c76afcc8ce01a969c8965f7b3ae1ba
NeeteshNG <neeteshng15@gmail.com>
2024-02-21 15:04:54 +0530

    Wishlist Working fine

--- backend_api/wishlist/urls.py
+++ backend_api/wishlist/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import WishlistItemListCreateView, WishlistItemDetailView, WishlistItemDeleteView
+from .views import WishlistItemListCreateView, WishlistItemDeleteView
 
 urlpatterns = [
-    path('wishlist-items/', WishlistItemListCreateView.as_view(), name='wishlist-item-list'),
-    path('wishlist-items/<int:pk>/', WishlistItemDetailView.as_view(), name='wishlist-item-detail'),
+    path('wishlist-items/', WishlistItemListCreateView.as_view(), name='wishlist-item-list-create'),
     path('wishlist-items/delete/<int:pk>/', WishlistItemDeleteView.as_view(), name='wishlist-item-delete'),
 ]
