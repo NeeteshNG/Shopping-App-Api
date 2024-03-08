a89cf7f8647ef359ce412344fbb937813078cbbc
NeeteshNG <neeteshng15@gmail.com>
2024-03-08 17:07:06 +0530

    OTP on Register Works

--- backend_api/accounts/models.py
+++ backend_api/accounts/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from django.utils import timezone
+import datetime
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
@@ -10,3 +11,13 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+class OTP(models.Model):
+    email = models.EmailField()
+    otp = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField(default=lambda: timezone.now() + datetime.timedelta(minutes=5))
+
+    @property
+    def is_expired(self):
+        return timezone.now() > self.expires_at
--- backend_api/backend_api/settings.py
+++ backend_api/backend_api/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'products',
     'cart',
-    'wishlist'
+    'wishlist',
 ]
 
 MIDDLEWARE = [
@@ -146,3 +146,10 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587  # Port for SMTP (587 for TLS, 465 for SSL)
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'sendotp1410@gmail.com' 
+EMAIL_HOST_PASSWORD = 'tcem lzyj djle jmuv' 
